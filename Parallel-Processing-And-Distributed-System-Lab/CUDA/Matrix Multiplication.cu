c7a1d252714c24ffcb5d0c64a0746d265f00ff06
RahatSiddique2001 <rahatsiddique2001@gmail.com>
2025-05-05 02:02:35 +0600

    cudaMatMulUpdated

--- Parallel-Processing-And-Distributed-System-Lab/CUDA/Matrix Multiplication.cu
+++ Parallel-Processing-And-Distributed-System-Lab/CUDA/Matrix Multiplication.cu
@@ -1,128 +1,82 @@
-#include <bits/stdc++.h>
-#include <cuda.h>
-
+%%writefile asif.cu
+#include <iostream>
+#include <cuda_runtime.h>
 using namespace std;
 
-const int K = 9; //... Total number of matrix
-const int M = 5; //... Row of 1st matrix
-const int N = 3; //... Column of 1st matrix & row of 2nd matrix
-const int P = 4; //... Column of 2nd matrix
+__global__ void matrixMul(float* A, float* B, float* C, int M, int N, int P, int offset) {
+    int k = threadIdx.x + offset;
 
-__global__ void multiplyMatrix(int *x, int *y, int *z, int M, int N, int P)
-{
-    int r = blockIdx.y * blockDim.y + threadIdx.y;
-    int c = blockIdx.x * blockDim.x + threadIdx.x;
+    float* a = A + k * M * N;
+    float* b = B + k * N * P;
+    float* c = C + k * M * P;
 
-    if (r < M and c < P)
-    {
-        int ans = 0;
-        for (int k = 0; k < N; k++)
-        {
-            ans += x[r * N + k] * y[k * P + c];
-        }
-        z[r * P + c] = ans;
-    }
-}
-
-void generateRandomMatrix(int *matrix, int M, int N)
-{
-    for (int r = 0; r < M; r++)
-    {
-        for (int c = 0; c < N; c++)
-        {
-            matrix[r * N + c] = rand() % 10;
+    for(int i = 0; i < M; i++) {
+        for(int j = 0; j < N; j++) {
+            for(int l = 0; l < P; l++) {
+                //c[i][l] += a[i][j] * b[j][l];
+                c[i * P + l] += a[i * N + j] * b[j * P + l];
+            }
         }
     }
 }
 
-void printMatrix(int *matrix, int M, int N, int id)
-{
-    cout << "Result " << id << " :\n";
-    for (int r = 0; r < M; r++)
-    {
-        for (int c = 0; c < N; c++)
-        {
-            cout << matrix[r * N + c] << "\t";
-        }
-        cout << "\n";
+int main(int argc, char *argv[]) {
+
+    int T = atoi(argv[1]); //koyta thread use korte parbo
+    int K = atoi(argv[2]); //koita matrix gun
+
+    //100 gun, thread 10,
+
+    int M = 400, N = 400, P = 400;
+
+    int SizeA = M * N * K;
+    int SizeB = N * P * K;
+    int SizeC = M * P * K;
+
+    //memory alocate (cpu allocate)
+    float *h_A = new float[SizeA];
+    float *h_B = new float[SizeB];
+    float *h_C = new float[SizeC];
+
+
+    //malloc (gpu allocate)
+    float *d_A;
+    cudaMalloc(&d_A, SizeA * sizeof(float));
+    float *d_B;
+    cudaMalloc(&d_B, SizeB * sizeof(float));
+    float *d_C;
+    cudaMalloc(&d_C, SizeC * sizeof(float));
+
+    //data initialize
+    for (int i = 0; i < SizeA; i++) {
+        h_A[i] = rand();
     }
-    cout << "\n";
-}
-
-int main()
-{
-    int *X; //... Host 1st matrix
-    int *Y; //... Host 2nd matrix
-    int *Z; //... Host ans matrix
-
-    //... Host memoty allocation
-    X = (int *)malloc(K * M * N * sizeof(int));
-    Y = (int *)malloc(K * N * P * sizeof(int));
-    Z = (int *)malloc(K * M * P * sizeof(int));
-
-    srand(time(nullptr));
-
-    //... 1st & 2nd matrix generation
-    for (int k = 0; k < K; k++)
-    {
-        generateRandomMatrix(X + k * M * N, M, N);
-        generateRandomMatrix(Y + k * N * P, N, P);
-    }
-
-    int *x; //... Device 1st matrix
-    int *y; //... Device 2nd matrix
-    int *z; //... Device ans matrix
-
-    //... Device memory allocation
-    cudaMalloc((void **)&x, K * M * N * sizeof(int));
-    cudaMalloc((void **)&y, K * N * P * sizeof(int));
-    cudaMalloc((void **)&z, K * M * P * sizeof(int));
-
-    cudaEvent_t start; //... Start time
-    cudaEventCreate(&start); 
-
-    cudaEvent_t end; //... End time
-    cudaEventCreate(&end);
-
-    //... Copy data from host to device
-    cudaMemcpy(x, X, K * M * N * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(y, Y, K * N * P * sizeof(int), cudaMemcpyHostToDevice);
-
-    //... Define grid dimension & block dimension of thread
-    dim3 blockDim(16, 16);
-    dim3 gridDim((P + blockDim.x - 1) / blockDim.x, (M + blockDim.y - 1) / blockDim.y);
-
-    cudaEventRecord(start);
-
-    //... Launch the matrix multiplication kernel for each pair of matrices
-    for (int k = 0; k < K; k++)
-    {
-        multiplyMatrix<<<gridDim, blockDim>>>(x + k * M * N, y + k * N * P, z + k * M * P, M, N, P);
+    for(int i = 0; i < SizeB; i++) {
+        h_B[i] = rand();
     }
 
-    cudaEventRecord(end);
-    cudaEventSynchronize(end);
 
-    //... Copy the result back to the host
-    cudaMemcpy(Z, z, K * M * P * sizeof(int), cudaMemcpyDeviceToHost);
+    //copy from host to device
+    cudaMemcpy(d_A, h_A, SizeA * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, SizeB * sizeof(float), cudaMemcpyHostToDevice);
 
-    float time_taken = 0;
-    cudaEventElapsedTime(&time_taken, start, end);
-    cout << "Time taken to execute: " << time_taken << " ms\n\n";
+    //cuda process suru
+    int gunKorteHobe = K;
+    int offset = 0;
+    while(gunKorteHobe > 0){
 
-    //... Print the result
-    for (int k = 0; k < K; k++)
-    {
-        printMatrix(Z + k * M * P, M, P, k);
+        int currentBatch = min(gunKorteHobe, T);
+
+        matrixMul<<<1,currentBatch>>>(d_A, d_B, d_C, M, N, P, offset);
+        cudaDeviceSynchronize();
+
+        gunKorteHobe -= currentBatch;
+        offset += currentBatch;
     }
 
-    //... Free device and host memory
-    cudaFree(x);
-    cudaFree(y);
-    cudaFree(z);
-    free(X);
-    free(Y);
-    free(Z);
+    //let's copy back to cpu
+    cudaMemcpy(h_C, d_C, SizeC * sizeof(float), cudaMemcpyDeviceToHost);
 
-    return 0;
+    cout << "All operation done" << endl;
+
 }
